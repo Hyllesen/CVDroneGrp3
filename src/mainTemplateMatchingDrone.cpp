dc4a131b25e10a344f862851376eb55f22b9553e
Hyllesen <stefanhyltoft@gmail.com>
2016-06-09 15:16:23 +0200

    Add files via upload
    Template matching examples (Drone example virker ikke) Dur ikke.

---
+++ src/mainTemplateMatchingDrone.cpp
@@ -0,0 +1,150 @@
+#include "ardrone/ardrone.h"
+
+// --------------------------------------------------------------------------
+// main(Number of arguments, Argument values)
+// Description  : This is the entry point of the program.
+// Return value : SUCCESS:0  ERROR:-1
+// --------------------------------------------------------------------------
+
+using namespace std;
+using namespace cv;
+
+/// Global Variables
+Mat img; Mat templ; Mat result;
+char* image_window = "Source Image";
+char* result_window = "Result window";
+
+int match_method;
+int max_Trackbar = 5;
+
+cv::Mat image;
+
+
+int main(int argc, char *argv[])
+{
+	/// Create windows
+	//namedWindow(image_window, CV_WINDOW_AUTOSIZE);
+	//namedWindow(result_window, CV_WINDOW_AUTOSIZE);
+
+	/// Create Trackbar
+	//char* trackbar_label = "Method: \n 0: SQDIFF \n 1: SQDIFF NORMED \n 2: TM CCORR \n 3: TM CCORR NORMED \n 4: TM COEFF \n 5: TM COEFF NORMED";
+	//createTrackbar(trackbar_label, image_window, &match_method, max_Trackbar, MatchingMethod);
+
+	//Template matchin calls
+	templ = imread("C:\\templ.JPG", 1);
+
+	cout << templ.cols << " cols " << templ.rows << " rows " << endl;
+
+    // AR.Drone class
+    ARDrone ardrone;
+	
+    // Initialize
+    if (!ardrone.open()) {
+        std::cout << "Failed to initialize." << std::endl;
+        return -1;
+    }
+
+    // Battery
+    std::cout << "Battery = " << ardrone.getBatteryPercentage() << "[%]" << std::endl;
+
+    // Instructions
+    std::cout << "***************************************" << std::endl;
+    std::cout << "*       CV Drone sample program       *" << std::endl;
+    std::cout << "*           - How to play -           *" << std::endl;
+    std::cout << "***************************************" << std::endl;
+    std::cout << "*                                     *" << std::endl;
+    std::cout << "* - Controls -                        *" << std::endl;
+    std::cout << "*    'Space' -- Takeoff/Landing       *" << std::endl;
+    std::cout << "*    'Up'    -- Move forward          *" << std::endl;
+    std::cout << "*    'Down'  -- Move backward         *" << std::endl;
+    std::cout << "*    'Left'  -- Turn left             *" << std::endl;
+    std::cout << "*    'Right' -- Turn right            *" << std::endl;
+    std::cout << "*    'Q'     -- Move upward           *" << std::endl;
+    std::cout << "*    'A'     -- Move downward         *" << std::endl;
+    std::cout << "*                                     *" << std::endl;
+    std::cout << "* - Others -                          *" << std::endl;
+    std::cout << "*    'C'     -- Change camera         *" << std::endl;
+    std::cout << "*    'Esc'   -- Exit                  *" << std::endl;
+    std::cout << "*                                     *" << std::endl;
+    std::cout << "***************************************" << std::endl;
+
+    while (1) {
+        // Key input
+        int key = cv::waitKey(33);
+        if (key == 0x1b) break;
+
+        // Get an image
+        image = ardrone.getImage();
+
+		/*Template matching start*/
+		/// Source image to display
+		Mat img_display;
+		image.copyTo(img_display);
+
+		/// Create the result matrix
+		int result_cols = img.cols - templ.cols + 1;
+		int result_rows = img.rows - templ.rows + 1;
+
+		result.create(result_rows, result_cols, CV_32FC1);
+
+		/// Do the Matching and Normalize
+
+		match_method = 0;
+		matchTemplate(img, templ, result, match_method);
+		normalize(result, result, 0, 1, NORM_MINMAX, -1, Mat());
+
+		/// Localizing the best match with minMaxLoc
+		double minVal; double maxVal; Point minLoc; Point maxLoc;
+		Point matchLoc;
+
+		minMaxLoc(result, &minVal, &maxVal, &minLoc, &maxLoc, Mat());
+
+		/// For SQDIFF and SQDIFF_NORMED, the best matches are lower values. For all the other methods, the higher the better
+		if (match_method == CV_TM_SQDIFF || match_method == CV_TM_SQDIFF_NORMED)
+		{
+			matchLoc = minLoc;
+		}
+		else
+		{
+			matchLoc = maxLoc;
+		}
+
+		/// Show me what you got
+		rectangle(img_display, matchLoc, Point(matchLoc.x + templ.cols, matchLoc.y + templ.rows), Scalar::all(0), 2, 8, 0);
+		rectangle(result, matchLoc, Point(matchLoc.x + templ.cols, matchLoc.y + templ.rows), Scalar::all(0), 2, 8, 0);
+
+		/*Template matching end */
+
+        // Take off / Landing 
+        if (key == ' ') {
+            if (ardrone.onGround()) ardrone.takeoff();
+            else                    ardrone.landing();
+        }
+
+        // Move
+        double vx = 0.0, vy = 0.0, vz = 0.0, vr = 0.0;
+        if (key == 'i' || key == CV_VK_UP)    vx =  1.0;
+        if (key == 'k' || key == CV_VK_DOWN)  vx = -1.0;
+        if (key == 'u' || key == CV_VK_LEFT)  vr =  1.0;
+        if (key == 'o' || key == CV_VK_RIGHT) vr = -1.0;
+        if (key == 'j') vy =  1.0;
+        if (key == 'l') vy = -1.0;
+        if (key == 'q') vz =  1.0;
+        if (key == 'a') vz = -1.0;
+        ardrone.move3D(vx, vy, vz, vr);
+
+        // Change camera
+        static int mode = 0;
+        if (key == 'c') ardrone.setCamera(++mode % 4);
+
+        // Display the image
+        cv::imshow("camera", image);
+		imshow(result_window, result);
+
+    }
+
+    // See you
+    ardrone.close();
+
+    return 0;
+}
